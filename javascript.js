d0585938137b78ec5496ff4301c19a4921a07781
KayBrooks-6 <kay--brooks@hotmail.com>
2025-05-08 18:14:14 -0400

    feat: Built function to take the user's choices and call one of the operator functions, and set the three choice variables as null
    The three variables were set to null as they haven't been defined yet by the user. Those values will be provided by the event listeners that I will place yet, which will call  upon the function operate which will call the operaters  and store the result in a variable (result).

--- javascript.js
+++ javascript.js
@@ -1,29 +1,47 @@
-const add = function (a, b) {
-    return a + b;
-  };
-  
-const subtract = function (a, b) {
-    return a - b;
-  };
-  
-const sum = function (array) {
-    return array.reduce((total, current) => total + current, 0);
-  };
-  
-const multiply = function (array) {
-    return array.reduce((product, current) => product * current)
-  };
+const add = function(a, b) {
+  return a + b;
+};
 
+const subtract = function(a, b) {
+  return a - b;
+};
 
-  const factorial = function(n) {
-    let product = 1;
-    while (n > 1) {
-      product *= n;
-      n--;
-    }
-    return product;
-  };
+const multiply = function(a, b) {
+  return a * b;
+};
 
-const firstValue = 0;
-const secondValue = 0;
-const operator = '';
+const divide = function(a, b) {
+  if (b === 0) return 'Error: Divide by zero';
+  return a / b;
+};
+
+const factorial = function(n) {
+  let product = 1;
+  while (n > 1) {
+    product *= n;
+    n--;
+  }
+  return product;
+};
+
+function operate(firstValue, secondValue, operator) {
+  if (operator === 'add') {
+    return add(firstValue, secondValue);
+  } else if (operator === 'subtract') {
+    return subtract(firstValue, secondValue);
+  } else if (operator === 'multiply') {
+    return multiply(firstValue, secondValue);
+  } else if (operator === 'divide') {
+    return divide(firstValue, secondValue);
+  } else {
+    return 'Invalid operator';
+  }
+};
+
+let firstValue = null;
+let secondValue = null;
+let operator = null;
+
+let result = operate(firstValue, secondValue, operator);
+console.log(result);
+
