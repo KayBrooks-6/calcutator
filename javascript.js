0975b19730a978a0f8c850a349c0179903d152c4
KayBrooks-6 <kay--brooks@hotmail.com>
2025-05-08 15:52:37 -0400

    Build starting of project where a simple hmtl was made with references to the javascript page. On the js,  5 functions were made for the add, subtract, sum, multiply and dividing operations.

---
+++ javascript.js
@@ -0,0 +1,29 @@
+const add = function (a, b) {
+    return a + b;
+  };
+  
+const subtract = function (a, b) {
+    return a - b;
+  };
+  
+const sum = function (array) {
+    return array.reduce((total, current) => total + current, 0);
+  };
+  
+const multiply = function (array) {
+    return array.reduce((product, current) => product * current)
+  };
+
+
+  const factorial = function(n) {
+    let product = 1;
+    while (n > 1) {
+      product *= n;
+      n--;
+    }
+    return product;
+  };
+
+const firstValue = 0;
+const secondValue = 0;
+const operator = '';
